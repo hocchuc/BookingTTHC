4e2e4bdcf106aa4bbbead4d6183a34f7ae75ab9a
hocanhchuc <hocanhchuc@gmail.com>
2021-12-11 00:18:22 +0700

    * allow CORS, change init script

--- src/main/java/com/tthc/booking/BookingApplication.java
+++ src/main/java/com/tthc/booking/BookingApplication.java
@@ -20,10 +20,10 @@ public class BookingApplication {
 	public CommandLineRunner demo(BookingRepository repository) {
 		return (args) -> {
 			// save a few customers
-			repository.save(new Booking("Jack", "38 Le Lai", "3929382323", "Đổi giấy phép", "001", "accepted"));
-			repository.save(new Booking("Fang", "43 Le Lai", "3929382353", "Đổi giấy phép", "001", "done"));
-			repository.save(new Booking("Long", "64 Le Lai", "3929382635", "Đổi giấy phép", "001", "rejected"));
-			repository.save(new Booking("Ju", "23 Le Lai", "39293822344", "Đổi giấy phép", "001", "processing"));
+			repository.save(new Booking("Nguyễn Văn Hoàng", "38 Vĩnh Hưng, Hoàng Mai", "3929382323", "Đổi giấy phép Lái Xe", "048392", "Accepted"));
+			repository.save(new Booking("Trần Xuân Cung ", "43 Tôn Thất Tùng, Kim Liên, Đống Đa", "3929382353", "Làm Giấy Phép Xây Dựng", "8482990", "Done"));
+			repository.save(new Booking("Lê Thái Long", "64 Ngọc Hà, Ngọc Hồ, Ba Đình", "3929382635", "Làm giấy độc thân", "42349823", "Rejected"));
+			repository.save(new Booking("Nguyễn Trần Nhựt", "23 Nhật Tân, Tây Hồ", "39293822344", "Trích lục khai sinh", "025235231", "Processing"));
 
 			// fetch all customers
 			log.info("Booking found with findAll():");
@@ -32,24 +32,24 @@ public class BookingApplication {
 				log.info(booking.toString());
 			}
 			log.info("");
-
-			// fetch an individual customer by ID
-			Booking customer = repository.findById(1L);
-			log.info("Booking found with findById(1L):");
-			log.info("--------------------------------");
-			log.info(customer.toString());
-			log.info("");
-
-			// fetch customers by last name
-			log.info("Booking found with findByLastName('Bauer'):");
-			log.info("--------------------------------------------");
-			repository.findByFullName("Ju").forEach(bauer -> {
-				log.info(bauer.toString());
-			});
-			// for (Customer bauer : repository.findByLastName("Bauer")) {
-			//  log.info(bauer.toString());
-			// }
-			log.info("");
+//
+//			// fetch an individual customer by ID
+//			Booking customer = repository.findById(1L);
+//			log.info("Booking found with findById(1L):");
+//			log.info("--------------------------------");
+//			log.info(customer.toString());
+//			log.info("");
+//
+//			// fetch customers by last name
+//			log.info("Booking found with findByLastName('Bauer'):");
+//			log.info("--------------------------------------------");
+//			repository.findByFullName("Ju").forEach(bauer -> {
+//				log.info(bauer.toString());
+//			});
+//			// for (Customer bauer : repository.findByLastName("Bauer")) {
+//			//  log.info(bauer.toString());
+//			// }
+//			log.info("");
 		};
 	}
 }
