12e245b209d880d8890a73f0c540f2ee0e6133c9
hocanhchuc <hocanhchuc@gmail.com>
2021-12-11 00:27:53 +0700

    Add: crossorigin for web app

--- src/main/java/com/tthc/booking/data/BookingRepository.java
+++ src/main/java/com/tthc/booking/data/BookingRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
+
 @RepositoryRestResource(collectionResourceRel = "booking", path = "booking")
+@CrossOrigin("https://tthc.vercel.app/")
 public interface BookingRepository extends PagingAndSortingRepository<Booking, Long> {
 
     List<Booking> findByFullName(@Param("fullName") String fullName);
